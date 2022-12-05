33b8993023cb167e50f23845b638c8d96ede89f8
Jameson Chen <[jamesonychan@gmail.com]>
2022-12-05 15:58:30 -0500

    * Initiated springboot project

---
+++ src/main/java/personal/projects/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package personal.projects.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
