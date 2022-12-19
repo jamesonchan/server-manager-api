9a22d872b2049f5c445ed9401d88d324f3ff86b6
Jameson Chen <[jamesonychan@gmail.com]>
2022-12-19 16:14:12 -0500

    * Added Resource and Response

--- src/main/java/personal/projects/server/ServerApplication.java
+++ src/main/java/personal/projects/server/ServerApplication.java
@@ -1,13 +1,25 @@
 package personal.projects.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import personal.projects.server.enumeration.Status;
+import personal.projects.server.model.Server;
+import personal.projects.server.repo.ServerRepo;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+            serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+        };
+    }
 
 }
